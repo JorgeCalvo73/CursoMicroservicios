1f5ad84d3cb78944daf05b6220d14c90732fa75c
Jorge Calvo <calvoblancojorge@gmail.com>
2023-06-20 23:28:35 +0200

    Comunicación entre microservicios

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/controller/CustomerRestController.java
@@ -5,21 +5,37 @@
  */
 package com.paymentchain.customer.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.paymentchain.customer.entities.Customer;
+import com.paymentchain.customer.entities.CustomerProduct;
 import com.paymentchain.customer.respository.CustomerRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.Duration;
+import java.util.Collections;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  *
@@ -31,6 +47,30 @@ public class CustomerRestController {
     
     @Autowired
     CustomerRepository customerRepository;
+    
+    //Cliente web para consumir la API de otro microservicio. Sirve para la comunicación entre microservicios
+    private final WebClient.Builder webClientBuilder;
+    
+    public CustomerRestController(WebClient.Builder webClientBuilder){
+    
+        this.webClientBuilder = webClientBuilder;
+    }
+    
+    //webClient requires HttpClient library to work propertly       
+    HttpClient client = HttpClient.create()
+            //Connection Timeout: is a period within which a connection between a client and a server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            //Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain 
+            //period of time, while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
     
     @GetMapping()
     public List<Customer> list() {
@@ -70,6 +110,36 @@ public class CustomerRestController {
                   customerRepository.delete(findById.get());  
         }
         return ResponseEntity.ok().build();
+    }
+    
+    @GetMapping("/full")
+    public Customer getByCode(@RequestParam String code) {
+        //Pillamos el cliente
+        Customer customer = customerRepository.findByCode(code);
+        
+        //A cada uno de los productos del cliente le vamos a asignar el nombre del cliente
+        List<CustomerProduct> products = customer.getProducts();
+        products.forEach(x -> {
+            String productName = getProductName(x.getId());
+            x.setProductName(productName);
+        });
+        return customer;
+    }
+    
+    private String getProductName(long id) { 
+        //El cliente recibe la config del http client que hemos declarado al principio de la clase
+        WebClient build = webClientBuilder
+                .clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://localhost:8083/product")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8083/product"))
+                .build();
+        
+        JsonNode block = build.method(HttpMethod.GET).uri("/" + id)
+                .retrieve().bodyToMono(JsonNode.class).block();
+        
+        String name = block.get("name").asText();
+        return name;
     }
     
 }
